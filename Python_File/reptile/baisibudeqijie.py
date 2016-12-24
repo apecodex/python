2264ce357180dbbc84bc474a3295a5bdb84fd57b
rookiesmile <1473018671@qq.com>
2016-12-24 16:05:31 +0800

    Update baisibudeqijie.py

--- Python_File/reptile/baisibudeqijie.py
+++ Python_File/reptile/baisibudeqijie.py
@@ -88,7 +88,7 @@ def img():
                 img_Re = urllib.request.Request(download[1],headers=headers)
                 img_open = urllib.request.urlopen(img_Re)
                 print("Download... %s" % download[0][0])
-                urllib.request.urlretrieve(download[1],"baisibudeqijie_img//%s" % (download[0][0]+"."+download[1][-4:]))
+                urllib.request.urlretrieve(download[1],"baisibudeqijie_img//%s" % (download[0][0]+download[1][-5:]))
                 print("%s download complite!" % download[0][0])
             except FileNotFoundError as e:
                 print("图片链接没有找到,可能是被删除了吧～",e)
@@ -187,12 +187,72 @@ def audio():
         print("--------------------------------------")
         time+=1
 
+def peri():
+    time = 1
+    while True:
+        url = "http://www.budejie.com/tag/117/1"+str(time)
+        headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"}
+        url_Request = urllib.request.Request(url,headers=headers)
+        try:
+            url_open = urllib.request.urlopen(url_Request)
+        except urllib.error.HTTPError as f:
+            print("页面没有找到",f.code)
+        url_soup = BeautifulSoup(url_open.read().decode("utf-8"),'lxml')
+        try:
+            url_img = url_soup.find("div",class_="j-r-c").find_all("img")
+            url_video = url_soup.find("div",class_="j-r-c").find_all("div",class_=" j-video")
+            url_video_name = url_soup.find_all("li",class_="j-r-list-tool-l-down f-tar j-down-video j-down-hide ipad-hide")
+        except AttributeError as e:
+            print("已经爬完了，这个网页的页面不多～不信自己翻一翻")
+
+        img_link = []    # 图片的链接
+        img_name = []    # 图片的名字
+        video_link = []    # 视频的链接
+        video_name = []    # 视频的名字
+        for i in url_video:
+            video_link.append(Replace(i["data-mp4"]))
+        for video_names in url_video_name:
+            video_name.append(video_names["data-text"])
+        for i in url_img:
+            if "ugc" in i["data-original"]:
+                img_name.append(i["alt"])
+                img_link.append(i["data-original"])
+        print("----------------------------------------")
+        print("正在下载第 %s 页" % time)
+        print("----------------------------------------")
+        print("开始下载图片....")
+        for download in zip(img_name,img_link):
+            try:
+                print("Download... %s" % download[0])
+                urllib.request.urlretrieve(download[1],"baisibudeqijie_peri//baisibudeqijie_peri_img//%s" % (download[0]+download[1][-5:]))
+                print("%s Download complite!" % download[0])
+            except FileNotFoundError as e:
+                print("图片链接没有找到，可能是被删除了吧～",e)
+                pass
+            print("图片已经下载完成")
+            print("------------------------------------")
+            print("------------------------------------")
+            print("开始下载视频...")
+            for downloads in zip(video_name,video_link):
+                try:
+                    print("Download... %s" % downloads[0])
+                    urllib.request.urlretrieve(downloads[1],"baisibudeqijie_peri//baisibudeqijie_peri_video//%s" % (downloads[0]+downloads[1][-5:]))
+                    print("%s Download complite!" % downloads[0])
+                except FileNotFoundError as e:
+                    print("视频链接没有找到，可能是被删除了吧～",e)
+                    pass
+                print("视频已经下载完成...")
+                print("---------------------------------")
+        print("第 %s 页已经下载完成!" % time)
+        time+=1
+
 
 print("--------------------------------")
 print("| 1.video                      |")
 print("| 2.img                        |")
 print("| 3.text                       |")
 print("| 4.audio                      |")
+print("| 5.peri                       |")
 print("--------------------------------")
 user = input("请选择1-2-3-4: ")
 
@@ -236,6 +296,21 @@ elif user == "4":
     except FileExistsError:
         pass
     audio()
-
+elif user == "5":
+    print("正在获取页面...")
+    try:
+        os.mkdir("baisibudeqijie_peri")
+        os.mkdir("baisibudeqijie_peri//baisibudeqijie_peri_img")
+        os.mkdir("baisibudeqijie_peri//baisibudeqijie_peri_video") 
+        if os.path.exists("baisibudeqijie_peri") and os.path.exists("baisibudeqijie_peri_img") and os.path.exists("baisibudeqijie_peri_video"):
+            os.rmdir("baisibudeqijie_peri//baisibudeqijie_peri_img")
+            os.rmdir("baisibudeqijie_peri//baisibudeqijie_peri_video")
+            os.rmdir("baisibudeqijie_peri")
+            os.mkdir("baisibudeqijie_peri")
+            os.mkdir("baisibudeqijie_peri_img")
+            os.mkdir("baisibudeqijie_peri_video")
+    except FileExistsError:
+        pass
+    peri()
 else:
     print("抱歉,选项里没有 '%s'" % user)
