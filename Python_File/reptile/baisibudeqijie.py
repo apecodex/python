4596d1cad85650c456ebb8ecd4ecb940f7634bf9
rookiesmile <1473018671@qq.com>
2016-12-24 14:27:36 +0800

    Update baisibudeqijie.py

--- Python_File/reptile/baisibudeqijie.py
+++ Python_File/reptile/baisibudeqijie.py
@@ -42,9 +42,13 @@ def video():
         print("------------------------------------")
         print("正在下载第 %s 页" % time)
         for z in zip(replace_name,div):
-            print("Downloading... %s" % z[0])
-            urllib.request.urlretrieve(z[1],"baisibudeqijie_video//%s.mp4" % z[0])
-            print("%s Download complite!" % z[0])
+            try:
+                print("Downloading... %s" % z[0])
+                urllib.request.urlretrieve(z[1],"baisibudeqijie_video//%s.mp4" % z[0])
+                print("%s Download complite!" % z[0])
+            except FileNotFoundError as e:
+                print("视频链接没有找到,可能被删除了吧～",e)
+                pass
         print("第 %s 页已下载完成" % time)
         print("------------------------------------")
         time+=1
@@ -69,23 +73,28 @@ def img():
         for i in url_div:
             url_img.append(i["data-original"])
             url_name.append(i["alt"])
-        url_reght = []  # 这个网站的图片会有一些不是图片的链接出现，没有后缀的~下载下来也没用，把它去掉
+        url_reght = []   # 这个网站的图片会有一些不是图片的链接出现，没有后缀的~下载下来也没用，把它去掉
         a = [".gif",".png",".jpg"]
         for i in url_img:
             if i[-4:] in a:
                 url_reght.append(i)
-        url_name_split = []  # 因为字符串太长，储存的时候会报错，就截取'，'前面第一个
+        url_name_split = []    # 因为字符串太长，储存的时候会报错，就截取'，'前面第一个
         for name_split in url_name:
             url_name_split.append(name_split.split("，"))
         print("--------------------------------")
         print("正在下载第 %s 页" % time)
         for download in zip(url_name_split,url_reght):
-            img_Re = urllib.request.Request(download[1],headers=headers)
-            img_open = urllib.request.urlopen(img_Re)
-            print("Download... %s" % download[0][0])
-            urllib.request.urlretrieve(download[1],"baisibudeqijie_img//%s" % (download[0][0]+"."+download[1][-4:]))
-            print("%s download complite!" % download[0][0])
+            try:
+                img_Re = urllib.request.Request(download[1],headers=headers)
+                img_open = urllib.request.urlopen(img_Re)
+                print("Download... %s" % download[0][0])
+                urllib.request.urlretrieve(download[1],"baisibudeqijie_img//%s" % (download[0][0]+"."+download[1][-4:]))
+                print("%s download complite!" % download[0][0])
+            except FileNotFoundError as e:
+                print("图片链接没有找到,可能是被删除了吧～",e)
+                pass
         print("第 %s 页已经下载完成" % time)
+        print("--------------------------------")
         time+=1
 
 def text():
@@ -117,12 +126,65 @@ def text():
         print("-------------------------------")
         print("正在下载第 %s 页" % time)
         for download in zip(name_split,text):
-            print("Download... %s" % download[0][0])
-            with open("baisibudeqijie_text//%s" % (download[0][0]+download[0][1]+download[0][2]+".txt"),"w") as f:
-                f.write(download[1])
-            print("%s Download complite" % download[0][0])
+            try:
+                print("Download... %s" % download[0][0])
+                with open("baisibudeqijie_text//%s" % (download[0][0]+download[0][1]+download[0][2]+".txt"),"w") as f:
+                    f.write(download[1])
+                print("%s Download complite" % download[0][0])
+            except FileNotFoundError as e:
+                print("段子没有找到，可能是被删除了吧～",e)
         print("第 %s 页已经下载完成" % time)
         print("-------------------------------")
+        time+=1
+
+def audio():
+    time = 1
+    while True:
+        url = "http://www.budejie.com/audio/"+str(time)
+        headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36"}
+        url_Request = urllib.request.Request(url,headers=headers)
+        try:
+            url_open = urllib.request.urlopen(url_Request)
+        except urllib.error.HTTPError as f:
+            print("页面没有找到",f.code)
+        url_soup = BeautifulSoup(url_open.read().decode("utf-8"),'lxml')
+        try:
+            url_audio = url_soup.find("div",class_="j-r-c").find_all("div",class_=" j-audio")
+            url_name = url_soup.find("div",class_="j-r-c").find_all("div",class_="j-r-list-c-desc")
+        except AttributeError as e:
+            print("已经爬完了，这个网页的页面不多～不信自己翻一翻")
+
+        audio_link = []   # audio的链接
+        audio_name = []   # audio的名字
+        audio_split = []    # 名字太长会报错，分开
+        audio_relpace_n = []    # 有'\n'会报错，换掉
+        audio_relpace_r = []    # 有'\r'也会报错，换掉
+
+        for x in url_audio:
+            audio_link.append(x["data-mp3"])
+
+        for i in url_name:
+            audio_name.append(i.get_text().replace(" ",""))
+
+        for i in audio_name:
+            audio_split.append(i.split("，"))
+
+        for n in audio_split:
+            audio_relpace_n.append((n[0].replace("\n","")))
+        for r in  audio_relpace_n:
+            audio_relpace_r.append(r.replace("\r",""))
+        print("--------------------------------------")
+        print("正在下载第 %s 页" % time)
+        for download in zip(audio_relpace_r,audio_link):
+            try:
+                print("Download... %s" % download[0])
+                urllib.request.urlretrieve(download[1],"baisibudeqijie_audio//%s" % (download[0]+".mp3"))
+                print("%s Download complite!" % download[0])
+            except FileNotFoundError as e:
+                print("没有找到,可能是被删除了吧～",e)
+                pass
+        print("第 %s 页已经下载完成！" % time)
+        print("--------------------------------------")
         time+=1
 
 
@@ -130,13 +192,14 @@ print("--------------------------------")
 print("| 1.video                      |")
 print("| 2.img                        |")
 print("| 3.text                       |")
+print("| 4.audio                      |")
 print("--------------------------------")
-user = input("请选择1 or 2: ")
+user = input("请选择1-2-3-4: ")
 
 if user == "1":
     print("正在获取页面...")
     try:
-        os.mkdir("baisibudeqijie_video")              # 这里就是先创建，如果存在就删除，然后重新创建
+        os.mkdir("baisibudeqijie_video")
         if os.path.exists("baisibudeqijie_video"):
             os.rmdir("baisibudeqijie_video")
             os.mkdir("baisibudeqijie_video")
@@ -163,5 +226,16 @@ elif user == "3":
     except FileExistsError:
         pass
     text()
+elif user == "4":
+    print("正在获取页面...")
+    try:
+        os.mkdir("baisibudeqijie_audio")
+        if os.path.exists("baisibudeqijie_audio"):
+            os.rmdir("baisibudeqijie_audio")
+            os.mkdir("baisibudeqijie_audio")
+    except FileExistsError:
+        pass
+    audio()
+
 else:
     print("抱歉,选项里没有 '%s'" % user)
